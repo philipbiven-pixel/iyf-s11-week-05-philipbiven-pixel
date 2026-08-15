8edd61337ae08047fe1710ff640d7f813241c4b6
philipbiven-pixel <philipbiven@gmail.com>
2026-08-15 15:18:31 +0300

    Implement click and keydown event logging
    Added event listeners for click and keydown events to log details.

--- events.js
+++ events.js
@@ -1,1 +1,31 @@
+// ========================================
+// TASK 10.2 - EVENT OBJECT
+// ========================================
 
+document.addEventListener("click", function (event) {
+
+    console.log("Clicked element:", event.target);
+
+    console.log("Event type:", event.type);
+
+    console.log(
+        "Mouse position:",
+        event.clientX,
+        event.clientY
+    );
+});
+
+
+document.addEventListener("keydown", function (event) {
+
+    console.log("Key pressed:", event.key);
+
+    console.log("Key code:", event.code);
+
+    console.log("Shift pressed:", event.shiftKey);
+
+    console.log("Ctrl pressed:", event.ctrlKey);
+
+    console.log("Alt pressed:", event.altKey);
+
+});
